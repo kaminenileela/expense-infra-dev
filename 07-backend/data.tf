51a536bb449bae0f474303bc86f458787c196260
kaminenileela <kaminenileela1992@gmail.com>
2024-08-01 05:43:21 -0400

    expense-ansible-roles-tf

--- 07-backend/data.tf
+++ 07-backend/data.tf
@@ -8,6 +8,11 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   
 }
 
+data "aws_ssm_parameter" "vpc_id" {
+  name  = "/${var.project_name}/${var.environment}/vpc_id"
+  
+}
+
 data "aws_ami" "ami_info" {
 
     most_recent = true
