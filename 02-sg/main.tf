11ce031bbd19b8715fad957325421f5696744332
kaminenileela <kaminenileela1992@email.com>
2024-08-27 02:46:29 -0400

    expense

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -229,6 +229,16 @@ resource "aws_security_group_rule" "bastion_public" {
   security_group_id = module.sg_bastion.sg_id
 }
 
+resource "aws_security_group_rule" "backend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.sg_backend.sg_id
+}
+
+
 # resource "aws_security_group_rule" "frontend_public" {
 #   type              = "ingress"
 #   from_port         = 22
