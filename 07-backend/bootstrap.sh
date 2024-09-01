7241f794ec68e7b01c1793331832d5378597e8a2
kaminenileela <kaminenileela1992@email.com>
2024-09-01 16:15:25 -0400

    expense

--- 07-backend/bootstrap.sh
+++ 07-backend/bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 component=$1
 environment=$2
-dnf install ansible -y
+echo "installing ansible"
+dnf install ansible -y 
+echo "installing ansible exit status: $?"
+echo "installing pip"
 pip3.9 install botocore boto3
+echo "installing pip exit status: $?"
+echo "installing ansible-pull"
 ansible-pull -i localhost, -U https://github.com/kaminenileela/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
+echo "installing ansible-pull exit status: $?"
