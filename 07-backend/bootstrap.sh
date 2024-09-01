bb3b3e6f9f2d386bc23cecaa0759d4222632d585
kaminenileela <kaminenileela1992@email.com>
2024-09-01 06:35:08 -0400

    expense

--- 07-backend/bootstrap.sh
+++ 07-backend/bootstrap.sh
@@ -2,5 +2,5 @@
 component=$1
 environment=$2
 dnf install ansible -y
-pip 3.9 install botocore boto3
+pip3.9 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/kaminenileela/expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
